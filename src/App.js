276a9e82c42a61f2dd05a4fca7ccc5a8af34418d
Oliver <radiumdahal@gmail.com>
2022-06-08 14:30:21 +0545

    v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Tutorial from './components/Tutorials/Tutorial';
+import './styles/App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import { useQueryClient, QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import Login from './components/Login';
+import SignIn from './components/SignIn';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/Home';
+// const queryClient = new QueryClient()
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <QueryClientProvider client={queryClient}>
+    
+        <div className="App">
+          <Router>
+            <Routes>
+              <Route path='/' exact element={<Home />} />
+              <Route path='/signin' exact element={<SignIn />} />
+              <Route path='/login' exact element={<Login />} />
+              <Route path='/tutorial' exact element={<Tutorial />} />
+            </Routes>   
+          </Router>
+        </div>
+    // </QueryClientProvider>
+
   );
 }
 
